02588cf1f6fd70a810bc45c1731873f45672a3b5
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2022-02-14 17:15:23 +0100

    Update data_utils.py

--- src/bigacgan/data_utils.py
+++ src/bigacgan/data_utils.py
@@ -172,8 +172,11 @@ def train(dataset, generator, discriminator, recognizer, composite_gan, checkpoi
             r_loss_fake_stds.append(r_loss_fake_std)
             g_loss_stds.append(g_loss_std)
             alphas.append(alpha)
+            print("d_loss_list:")
             print(d_loss_list)
+            print("d_loss_list mean:")
             print(np.mean(d_loss_list))
+            print("d_loss_list mean as str:")
             print(str(np.mean(d_loss_list)))
         epoch_summary.write(str(np.mean(d_loss_list)) + ";" + str(np.mean(d_loss_real_list)) + ";" +
                             str(np.mean(d_loss_fake_list)) + ";" + str(np.mean(r_loss_real_list)) + ";" +
