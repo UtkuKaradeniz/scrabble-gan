995587760cb5c9ae0ad81a8d248105dba85386ea
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2022-02-15 23:56:35 +0100

    Update net_architecture.py

--- src/bigacgan/net_architecture.py
+++ src/bigacgan/net_architecture.py
@@ -172,9 +172,12 @@ def make_my_recognizer(input_dim, sequence_length, output_classes, restore=False
 
     model = tf.keras.Model(inputs=[inp_imgs, labels, input_length, label_length], outputs=loss_out)
     if restore:
-        path_to_rc_chekpoint = '/scrabble-gan/data/simpleHTR_TF2/checkpoints/ex02/'
-        #TODO:
-        load_status = model.load_weights(os.path.join(path_to_rc_chekpoint, '275/checkpoint'))
+        path_to_rc_checkpoint = os.path.join('/scrabble-gan/data/simpleHTR_TF2/checkpoints/ex02/', '275/checkpoint')
+        latest_checkpoint = tf.train.latest_checkpoint(path_to_rc_checkpoint)
+        # if model must be restored (for inference), there must be a snapshot
+        if restore and not latest_checkpoint:
+            raise Exception('No saved model found in: ' + path_to_rc_checkpoint)
+        load_status = model.load_weights(latest_checkpoint)
         load_status.assert_confirmed()
         print("Rec. Model from " + path_to_rc_checkpoint + "/275" + " loaded")
 
